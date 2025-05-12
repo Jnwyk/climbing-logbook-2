f2084e66aa3a9585aa1847e88d0fe85c52465640
Jnwyk <jan.nowak.personal@gmail.com>
2025-05-12 21:31:30 +0200

    add basic route routing methods

--- climbing-logbook-backend/src/routes/routes.controller.ts
+++ climbing-logbook-backend/src/routes/routes.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Param, Get, Post, Patch, Delete } from '@nestjs/common';
 import { RoutesService } from './routes.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('routes')
 export class RoutesController {
@@ -8,5 +9,25 @@ export class RoutesController {
     @Get()
     getAllRoutes(){
         return this.routesService.getAllRoutes();
+    }
+
+    @Get('/:id')
+    getOneRoute(@Param('id') id: string){
+        return this.routesService.getOneRoute(Number(id));
+    }
+
+    @Post()
+    createRoute(@Body() createRouteDto: Prisma.routeCreateInput){
+        return this.routesService.createRoute(createRouteDto);
+    }
+
+    @Patch('/:id')
+    updateRoute(@Param('id') id: string, @Body() updateRouteDto: Prisma.routeUpdateInput){
+        return this.routesService.updateRoute(Number(id), updateRouteDto);
+    }
+
+    @Delete('/:id')
+    deleteRoute(@Param('id') id: string){
+        return this.routesService.deleteRoute(Number(id));
     }
 }
--- climbing-logbook-backend/src/routes/routes.service.ts
+++ climbing-logbook-backend/src/routes/routes.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -7,5 +8,36 @@ export class RoutesService {
     
     async getAllRoutes(){
         return this.databaseService.route.findMany();
+    }
+
+    async createRoute(createRouteDto: Prisma.routeCreateInput){
+        return this.databaseService.route.create({
+            data: createRouteDto
+        })
+    }
+
+    async getOneRoute(routeId: number){
+        return this.databaseService.route.findUnique({
+            where: {
+                id: routeId
+            }
+        })
+    }
+
+    async updateRoute(routeId: number, updateRouteDto: Prisma.routeUpdateInput){
+        return this.databaseService.route.update({
+            where: {
+                id: routeId
+            },
+            data: updateRouteDto
+        })
+    }
+
+    async deleteRoute(routeId: number){
+        return this.databaseService.route.delete({
+            where: {
+                id: routeId
+            }
+        })
     }
 }
